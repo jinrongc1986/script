e6be2fb36a10cced8f782b5cd6d84cd89c07a652
jinrongc <toby6893086@163.com>
2017-03-08 22:12:49 +0800

    update xlsx_edit

--- xlsx/xlsx_edit.py
+++ xlsx/xlsx_edit.py
@@ -5,18 +5,26 @@
 import sys,time,os
 from openpyxl import Workbook
 from openpyxl import load_workbook
+from openpyxl.styles import Color,Fill,Font,PatternFill
 #星域提供的节点信息
 f=open('university.txt')
 lista=[]
-listb=[]
 for line in f.readlines():
 	lista.append(line.strip('\n'))
 #本地保存的节点信息
 wb = load_workbook('CDS设备信息.xlsx')
 sheetname='VPE'
 ws=wb[sheetname]
-row=len(ws.columns)
-print row
 #比对并写入excel
 for i in range(len(lista)):
-	print lista[i]
+#for i in range(1):
+	#print lista[i]
+	for row in range(2,90):
+		col=5
+		value=ws.cell(row=row,column=col).value
+		if value==lista[i]:
+			ws.cell(row=row,column=8).value='success'
+			greenFill = PatternFill(start_color='0032CD32',end_color='0032CD32',fill_type='solid')
+			ws.cell(row=row,column=8).fill = greenFill
+			break
+wb.save('123.xlsx')
