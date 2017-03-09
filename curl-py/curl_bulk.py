c075bdeb2a05f706166c84ff6b7f123996d9bd21
jinrongc <toby6893086@163.com>
2017-03-09 22:59:45 +0800

    curl_bulk

---
+++ curl-py/curl_bulk.py
@@ -0,0 +1,65 @@
+#-*- coding:utf-8 -*-
+import subprocess,urllib2
+import os,sys,re
+import time
+from threading import Thread
+import getopt
+import paramiko
+# 参数输入
+opts,arts = getopt.getopt(sys.argv[1:],"hlru:n:c:i:")
+url="http://20.20.20.4/test/1.mp4?1"
+cycles='1'
+total=1
+client='4'
+location="local"
+for op,value in opts:
+        if op=="-u":
+                url=value
+        elif op=="-n":
+                total=value
+		total=int(total)
+        elif op=="-l":
+                location="local"
+        elif op=="-r":
+                location="remote"
+        elif op=="-c":
+                client=value
+def curl_bulk_set(url,client,cycles):
+	# curl_loader参数设置
+	f=open(r"/home/script/curl-py/curl_base.conf","r+")
+	flist=[]
+	flist=f.readlines()
+	flist[2]='CLIENTS_NUM_MAX='+client+'\n'
+	flist[3]='CLIENTS_NUM_START='+client+'\n'
+	flist[9]='CYCLES_NUM='+cycles+'\n'
+	flist[14]='URL='+url+'\n'
+	f=open(r"/home/script/curl-py/curl_base.conf","w+")
+	f.writelines(flist)
+
+def curl_bulk_exec(location):
+	# 执行curl命令
+	if location=="local":
+		cmd = "/home/curl-loader-0.56/curl-loader -f /home/script/curl-py/curl_base.conf"
+		p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	elif location=="remote":
+		ssh = paramiko.SSHClient()
+		ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+		ssh.connect('30.30.32.4',port=22,username = 'root',password='123456',timeout=5)
+		cmd = '/home/curl-loader-0.56/curl-loader -f /home/script/curl-py/curl_base.conf'    #进入用户目录home
+		stdin,stdout,stderr = ssh.exec_command(cmd)
+if __name__ == "__main__":
+	ISOTIMEFORMAT='%Y-%m-%d %X'
+	print time.strftime( ISOTIMEFORMAT, time.localtime() )
+	print 'running...'
+	for i in range(1,total+1):
+		print 'round:%d'%i
+		url="http://20.20.20.4/test/%d.mp4?%d"%(i,i)
+		curl_bulk_set(url,client,cycles)
+		curl_bulk_exec(location)
+		time.sleep(10)
+		if i<1001:
+			print 'dup round:%d'%i
+			curl_bulk_exec(location)
+			time.sleep(10)
+	print 'finished'
+	print time.strftime( ISOTIMEFORMAT, time.localtime() )
