fa37cf48f40b853ebf35d1c6becd046ce8dddead
jinrongc <toby6893086@163.com>
2017-04-18 14:11:02 +0800

    update dns_check

--- work/dns_cnc_check.py
+++ work/dns_cnc_check.py
@@ -2,26 +2,16 @@
 import os,subprocess
 import sys,getopt
 import json
-x=0
-y=0
-faillist=[]
-def check_dns(cnc_domain,port=53):
-    cmd='dig %s @20.20.20.2 -p %d'%(cnc_domain,port)
-    p=subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
-    stdout=p.stdout.read()
-    global y
-    global x
-    global faillist
-    if '192.168.1.' in stdout:
-        y = y + 1
-        return '%s is OK'%(cnc_domain)
-    else:
-        x = x + 1
-        faillist.append('failed:%s'%cnc_domain)
-        return '###########################%s is NOK'%cnc_domain
-#默认选择
+
+#默认参数选择
 filedomain='domain_cnc_video_demand.txt'
 port=53
+x=0
+y=0
+z=0
+fail_noanswer=[]
+fail_notlocal=[]
+
 #参数选择
 opts,arts = getopt.getopt(sys.argv[1:],"lvhp:")
 for op,value in opts:
@@ -35,6 +25,26 @@ for op,value in opts:
         port=value
         port=int(port)
 
+def check_dns(cnc_domain,port=53):
+    cmd='dig %s @192.168.1.109 -p %d'%(cnc_domain,port)
+    p=subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+    stdout=p.stdout.read()
+    global x
+    global y
+    global z
+    global faillist
+    if '192.168.1.17' in stdout:
+        x = x + 1
+        return '%s is OK'%(cnc_domain)
+    elif 'ANSWER: 0, AUTHORITY' in stdout:
+        y = y + 1
+        fail_noanswer.append('%s'%cnc_domain)
+        return '~~~~~~~~~~~~~~~~~~~~~~~~~~~%s is NoAnswer'%cnc_domain
+    else:
+        z = z + 1
+        fail_notlocal.append('%s'%cnc_domain)
+        return '###########################%s is NotLocal'%cnc_domain
+
 f=open(r"/home/git/script/work/%s"%filedomain,"r+")
 domainlist=[]
 domainlist=f.readlines()
@@ -43,6 +53,9 @@ for i in range(dllen):
     domain=domainlist[i].split('\n')[0]
     result=check_dns(domain,port)
     print result
-print 'total sucess : %d'%y
-print 'total fail :%d'%x
-print faillist
+print 'total domain counter : %d'%dllen
+print 'total sucess : %d'%x
+print 'total not local : %d'%z
+print fail_notlocal
+print 'total no answer : %d'%y
+print fail_noanswer
