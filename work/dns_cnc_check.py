bd12eb5e38137e96e1e4cdad496194431af6fe66
jinrongc <toby6893086@163.com>
2017-04-18 11:57:50 +0800

    update dns_check

--- work/dns_cnc_check.py
+++ work/dns_cnc_check.py
@@ -5,8 +5,8 @@ import json
 x=0
 y=0
 faillist=[]
-def check_dns(cnc_domain):
-    cmd='dig %s @20.20.20.2'%cnc_domain
+def check_dns(cnc_domain,port=53):
+    cmd='dig %s @20.20.20.2 -p %d'%(cnc_domain,port)
     p=subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
     stdout=p.stdout.read()
     global y
@@ -21,15 +21,19 @@ def check_dns(cnc_domain):
         return '###########################%s is NOK'%cnc_domain
 #默认选择
 filedomain='domain_cnc_video_demand.txt'
+port=53
 #参数选择
-opts,arts = getopt.getopt(sys.argv[1:],"lvh")
+opts,arts = getopt.getopt(sys.argv[1:],"lvhp:")
 for op,value in opts:
-        if op=="-l":
-                filedomain='domain_cnc_video_live.txt'
-        elif op=="-v":
-                filedomain='domain_cnc_video_demand.txt'
-        elif op=="-h":
-                filedomain='domain_cnc_http.txt'
+    if op=="-l":
+        filedomain='domain_cnc_video_live.txt'
+    elif op=="-v":
+        filedomain='domain_cnc_video_demand.txt'
+    elif op=="-h":
+        filedomain='domain_cnc_http.txt'
+    elif op=="-p":
+        port=value
+        port=int(port)
 
 f=open(r"/home/git/script/work/%s"%filedomain,"r+")
 domainlist=[]
@@ -37,7 +41,7 @@ domainlist=f.readlines()
 dllen=len(domainlist)
 for i in range(dllen):
     domain=domainlist[i].split('\n')[0]
-    result=check_dns(domain)
+    result=check_dns(domain,port)
     print result
 print 'total sucess : %d'%y
 print 'total fail :%d'%x
