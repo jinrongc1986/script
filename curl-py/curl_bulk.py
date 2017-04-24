995050b1459a1b9a189fa91ec18a9245d124a122
jinrongc <toby6893086@163.com>
2017-04-24 15:19:53 +0800

    update curl_bulk\

--- curl-py/curl_bulk.py
+++ curl-py/curl_bulk.py
@@ -7,18 +7,23 @@ from threading import Thread
 import getopt
 import paramiko
 # 参数输入
-opts,arts = getopt.getopt(sys.argv[1:],"hlru:t:n:c:")
+opts,arts = getopt.getopt(sys.argv[1:],"hlru:t:s:n:c:S:N:")
 url="http://20.20.20.4/test/0001.mp4?0001"
 cycles='1'
 total=1
 client='3'
 location="local"
 filetype='mp4'
+start=1
+q_start=0
 for op,value in opts:
     if op=="-u":
         url=value
     elif op=="-t":
         filetype=value
+    elif op=="-s":
+        start=value
+        start=int(start)
     elif op=="-n":
         total=value
         total=int(total)
@@ -28,6 +33,12 @@ for op,value in opts:
         location="remote"
     elif op=="-c":
         client=value
+    elif op=="-S":
+        q_start=value
+        q_start=int(q_start)
+    elif op=="-N":
+        q_end=value
+        q_end=int(q_end)
     elif op=="-h":
         print "-u URL like 'http://20.20.20.4/test/'"
         print "-t filetype like 'mp4'"
@@ -51,7 +62,7 @@ def curl_bulk_set(url,client,cycles):
 def curl_bulk_exec(location):
     # 执行curl命令
     if location=="local":
-        cmd = "/home/curl-loader-0.56/curl-loader -f /home/git/script/curl-py/curl_base.conf"
+        cmd = "/home/git/curl-loader-0.56/curl-loader -f /home/git/script/curl-py/curl_base.conf"
         p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         #p = subprocess.Popen(cmd, shell=True)
         #p.communicate(input=None)
@@ -66,17 +77,32 @@ if __name__ == "__main__":
     ISOTIMEFORMAT='%Y-%m-%d %X'
     print time.strftime( ISOTIMEFORMAT, time.localtime() )
     print 'running...'
-    for i in range(506,total+1):
-        print 'round:%d'%i
-        a=str(i).zfill(4)
-        url="http://20.20.20.4/test/%s.%s?%s"%(a,filetype,a)
-        curl_bulk_set(url,client,cycles)
-        curl_bulk_exec(location)
-        #time.sleep(0.2)
-        if 1000<i<2001:
-            print 'dup round:%d'%i
-            curl_bulk_set(url,'4',cycles)
+    if q_start==0:
+        for i in range(start,total+1):
+            print 'round:%d'%i
+            a=str(i).zfill(4)
+            url="http://20.20.20.4/test/%s.%s?%s"%(a,filetype,a)
+            curl_bulk_set(url,client,cycles)
             curl_bulk_exec(location)
-            time.sleep(0.2)
+            #time.sleep(0.2)
+            if 1000<i<2001:
+                print 'dup round:%d'%i
+                curl_bulk_set(url,'4',cycles)
+                curl_bulk_exec(location)
+                time.sleep(0.2)
+    else :
+        for i in range(start,total+1):
+            print 'round:%d'%i
+            a=str(i).zfill(4)
+            for x in range(q_start,q_end+1):
+                url="http://20.20.20.4/test/%s.%s?%s"%(a,filetype,x)
+                curl_bulk_set(url,client,cycles)
+                curl_bulk_exec(location)
+                #time.sleep(0.2)
+                if 1000<i<2001:
+                    print 'dup round:%d'%i
+                    curl_bulk_set(url,'4',cycles)
+                    curl_bulk_exec(location)
+                    time.sleep(0.2)
     print 'finished'
     print time.strftime( ISOTIMEFORMAT, time.localtime() )
