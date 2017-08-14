80c7acbc404abc1b4acf201934083ffb509b5e44
jinrongc1986 <toby6893086@163.com>
2017-08-14 23:34:23 +0800

    update

--- appleid/CloudId.py
+++ appleid/CloudId.py
@@ -31,7 +31,6 @@ def create_cloudid(mailname,mailpasswd):
     option = webdriver.ChromeOptions()
     option.add_argument('--user-agent=%s'%ua)
     option.add_argument('--proxy-server=%s' % proxy)
-    option.add_argument(')
     driver = webdriver.Chrome(chrome_options=option)
     # driver=webdriver.Firefox()
     driver.get("https://www.icloud.com/")
