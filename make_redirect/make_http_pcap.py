bdfbcb1ae6e2a8d3c251586fd6077998b1f7c901
jinrongc1986 <toby6893086@163.com>
2017-08-31 16:20:45 +0800

    格式化

--- make_redirect/make_http_pcap.py
+++ make_redirect/make_http_pcap.py
@@ -1,58 +1,62 @@
-#-*- coding:utf-8 -*-
-import os,subprocess,time
+# -*- coding:utf-8 -*-
+import os, subprocess, time
 from time import sleep
 import json
 
 
 def getlocalurls():
-    print ("读取本地数据库...")
-    cmd='rm -f test_jinrongc.txt'
-    cmd1='mysql -N  -e  "select uri from cache.video_cache ;" >> test_jinrongc.txt'
-    cmd2='mysql -N  -e  "select uri from cache.mobile_cache ;" >> test_jinrongc.txt'
-    cmd3='mysql -N  -e  "select uri from cache.http_cache ;" >> test_jinrongc.txt'
-    cmd4='mv test_jinrongc.txt urls.txt'
+    print("读取本地数据库...")
+    cmd = 'rm -f test_jinrongc.txt'
+    cmd1 = 'mysql -N  -e  "select uri from cache.video_cache ;" >> test_jinrongc.txt'
+    cmd2 = 'mysql -N  -e  "select uri from cache.mobile_cache ;" >> test_jinrongc.txt'
+    cmd3 = 'mysql -N  -e  "select uri from cache.http_cache ;" >> test_jinrongc.txt'
+    cmd4 = 'mv test_jinrongc.txt urls.txt'
     p = subprocess.call(cmd, shell=True)
     p1 = subprocess.call(cmd1, shell=True)
     p2 = subprocess.call(cmd2, shell=True)
     p3 = subprocess.call(cmd3, shell=True)
     p4 = subprocess.call(cmd4, shell=True)
 
+
 def make_http_pcap(flag='all'):
     getlocalurls();
-    print ("开始抓包...")
-    capture="tcpdump -i eth0 'tcp[((tcp[12:1] & 0xf0) >> 2):4] = 0x48454144' -w http_head.pcap"
+    print("开始抓包...")
+    capture = "tcpdump -i eth0 'tcp[((tcp[12:1] & 0xf0) >> 2):4] = 0x48454144' -w http_head.pcap"
     dump = subprocess.Popen(capture, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    f=open('./urls.txt','r')
-    urls=f.readlines()
-    print ("开始发流...")
+    f = open('./urls.txt', 'r')
+    urls = f.readlines()
+    print("开始发流...")
     for url in urls:
-        url=url.strip()
-        if flag=='all':
-            cmd="wget '%s' --spider"%url
+        url = url.strip()
+        if flag == 'all':
+            cmd = "wget '%s' --spider" % url
             p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        elif flag=='cnc':
+        elif flag == 'cnc':
             if url_analysis(url):
-                cmd="wget '%s' --spider"%url
+                cmd = "wget '%s' --spider" % url
                 p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     sleep(5)
     dump.terminate()
     os.system('rm -f urls.txt')
-    print ("生成最新的http_head.pcap成功")
+    print("生成最新的http_head.pcap成功")
+
 
 def url_analysis(url):
-    uri=url.split('//')[1].split('?')[0]
-    host=uri.split('/')[0]
-    f=open('/home/cnc_export/cnc_host.conf','r')
+    uri = url.split('//')[1].split('?')[0]
+    host = uri.split('/')[0]
+    f = open('/home/cnc_export/cnc_host.conf', 'r')
     data = json.load(f)
-    wlist=data["white_portal"]
+    wlist = data["white_portal"]
     for white in wlist:
-        try :
-            if white == host :
+        try:
+            if white == host:
                 return True
         except Exception as e:
-            print e
+            print
+            e
             pass
     return False
 
-if __name__=='__main__' :
+
+if __name__ == '__main__':
     make_http_pcap()
--- make_redirect/make_redirect.py
+++ make_redirect/make_redirect.py
@@ -1,7 +1,8 @@
-#-*- coding:utf-8 -*-
-import os,subprocess,time
+# -*- coding:utf-8 -*-
+import os, subprocess, time
 from time import sleep
 from xedge.tool import make_http_pcap
+
 
 def create_bridge():
     os.system('brctl addbr br-xedge')
@@ -12,47 +13,56 @@ def create_bridge():
     os.system('ifconfig br-xedge up')
     os.system('ifconfig veth0 up')
     os.system('ifconfig veth1 up')
-    print ("创建bridge成功")
+    print("创建bridge成功")
+
 
 def get_local_ip(nic='br100'):
-    ip = os.popen(("ifconfig %s | grep 'inet addr:' | grep -v '127.0.0.1' | cut -d: -f2 | awk '{print $1}' | head -1")%nic).read().split('\n')[0] 
-    print ("%s ip : %s"%(nic,ip))
+    ip = os.popen((
+                  "ifconfig %s | grep 'inet addr:' | grep -v '127.0.0.1' | cut -d: -f2 | awk '{print $1}' | head -1") % nic).read().split(
+        '\n')[0]
+    print("%s ip : %s" % (nic, ip))
     return ip
 
-def tcprew(ip):
-    iplist=ip.split('.')
-    iplist[3]= str(int(iplist[3])+1)
-    ipnew='.'.join(iplist)
-    os.system('tcprewrite --srcipmap=%s:%s -i http_head.pcap -o bigFlows.pcap'%(ip,ipnew))
-    os.system('rm -f http_head.pcap')
-    print ("重写 src_ip 为 : %s"%ipnew)
 
-def tcprep(speed=1,loop=2):
-    os.system('tcpreplay -K -i veth1 --mbps %d  --loop %d  --unique-ip bigFlows.pcap'%(speed,loop))
+def tcprew(ip):
+    iplist = ip.split('.')
+    iplist[3] = str(int(iplist[3]) + 1)
+    ipnew = '.'.join(iplist)
+    os.system('tcprewrite --srcipmap=%s:%s -i http_head.pcap -o bigFlows.pcap' % (ip, ipnew))
+    os.system('rm -f http_head.pcap')
+    print("重写 src_ip 为 : %s" % ipnew)
+
+
+def tcprep(speed=1, loop=2):
+    os.system('tcpreplay -K -i veth1 --mbps %d  --loop %d  --unique-ip bigFlows.pcap' % (speed, loop))
     os.system('rm -f bigFlows.pcap')
-    print ("tcpreplay 执行成功，请确认重定向日志！")
+    print("tcpreplay 执行成功，请确认重定向日志！")
+
 
 def del_bridge():
     os.system('brctl delif br-xedge eth1')
-    print ("删除bridge 的 eth1成功")
-    
-def setbw(bw='1'):
-    print("设置回源带宽为%sM，以防内网崩塌")%bw
-    os.system("/home/icache/configd config set download bandwidth work '%s'"%bw)
-    print ('设置非空闲时段成功')
-    os.system("/home/icache/configd config set download bandwidth spare '%s'"%bw)
-    print ('设置空闲时间成功')
-    print ('等待20秒，等icached恢复')
-    sleep(20)
-    
-def main(flag='all'):
-    #制造所有资源的重定向为all,制造cnc_export重定向为cnc
-    make_http_pcap.make_http_pcap(flag) 
-    create_bridge()
-    br100_ip=get_local_ip(nic='br100')
-    tcprew(br100_ip)
-    tcprep(speed=1,loop=2)
-    del_bridge() 
+    print("删除bridge 的 eth1成功")
 
-if __name__=='__main__' :
+
+def setbw(bw='1'):
+    print("设置回源带宽为%sM，以防内网崩塌") % bw
+    os.system("/home/icache/configd config set download bandwidth work '%s'" % bw)
+    print('设置非空闲时段成功')
+    os.system("/home/icache/configd config set download bandwidth spare '%s'" % bw)
+    print('设置空闲时间成功')
+    print('等待20秒，等icached恢复')
+    sleep(20)
+
+
+def main(flag='all'):
+    # 制造所有资源的重定向为all,制造cnc_export重定向为cnc
+    make_http_pcap.make_http_pcap(flag)
+    create_bridge()
+    br100_ip = get_local_ip(nic='br100')
+    tcprew(br100_ip)
+    tcprep(speed=1, loop=2)
+    del_bridge()
+
+
+if __name__ == '__main__':
     main(flag='all')
