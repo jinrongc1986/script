fe25a25f7a24c4d11c6fbb294a118e3197a0cc6c
jinrongc1986 <toby6893086@163.com>
2017-10-06 21:00:07 +0800

    updata startup

--- appleid/startup.py
+++ appleid/startup.py
@@ -203,5 +203,5 @@ if __name__ == '__main__':
     count = 15
     for i in range (10) :
         need_money(mailname_pre, domain, mailpasswd, body, count)
-        print ("等待3小时")
-        sleep(10800)
+        print ("等待4小时")
+        sleep(14400)
