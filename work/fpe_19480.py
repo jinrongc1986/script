dd95aead8dba9d91faac5834f000b381f7847162
root <root@localhost.(none)>
2018-11-09 18:44:32 +0800

    update 19480

--- work/fpe_19480.py
+++ work/fpe_19480.py
@@ -4,25 +4,33 @@ import sys
 import xmlrpclib
 import time
 
+debug = 1
 XML_RPC = "http://localhost:19480/RPC2"
 g_xmlrpc_proxy = xmlrpclib.ServerProxy(XML_RPC)
 
 def date_now():
     timenow=time.localtime(time.time())
     return time.strftime("%Y-%m-%d",timenow)
+
 def insert_task(url):
+    global debug
     msg = {"nodeid": "101", "token": "3356", "action": "set_task", "var0": "1","var1":url}
+    print msg
     out = g_xmlrpc_proxy.callback(msg)
+    if debug :
+        print url
+        print out
     return out
     
 def bulk_insert():
+    global debug
     filename='/home/dcwork/precache/listC/%s_part-00000'%date_now()
     f = open(filename,'r')
     lines=f.readlines()
     for line in lines:
         url = line.split("\t")[-1].strip()
         out=insert_task(url)
-        if "'results': '1'" not in str(out):
+        if "'results': '1'" not in str(out) & debug:
             print '#'*20
             print url
             print out
